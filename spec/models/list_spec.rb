e0483d1bfb5afc515bddc9a5b9d73ef9ef8e0f5e
Miles Bradt <smbradtmichael@gmail.com>
2018-12-31 11:41:34 -0800

    runs test for checking that price works

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe List do
   it { should validate_presence_of :name }
-end
-
-describe List do
-  it { should validate_presence_of :price }
+  test_list = List.new({:price => "20.00"})
+  it 'returns the price set' do
+    expect(test_list.price).to eq "20.00"
+  end
 end
