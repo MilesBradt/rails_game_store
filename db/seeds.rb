5bacffba6cd15b7e12bf2e36651ac4e55e508463
Miles Bradt <smbradtmichael@gmail.com>
2019-01-01 14:24:39 -0800

    add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  sm64 = List.create({:name => "Super Mario 64 jp", :price => "$25.00 USD"})
+  pm = List.create({:name => "Paper Mario", :price => "$35.00 USD"})
+  sm = List.create({:name => "Super Metroid", :price => "$55.00 USD"})
